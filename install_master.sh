1a5dbe26485baba8034bf56931e3541048ff3fe7
vsaini44 <vishal@ap2v.com>
2021-06-10 12:54:37 +0530

    Update install_master.sh

--- install_master.sh
+++ install_master.sh
@@ -1,10 +1,12 @@
+
 #on master
 apt install kubectl -y
-kubeadm init --pod-network-cidr=10.244.0.0/16 >> cluster_initialized.txt
+#kubeadm init --pod-network-cidr=10.244.0.0/16 >> cluster_initialized.txt
+kubeadm init --pod-network-cidr=192.168.0.0/16 >> cluster_initialized.txt
 mkdir /root/.kube
 cp /etc/kubernetes/admin.conf /root/.kube/config
-kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
+#kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
 systemctl restart kubelet.service
 kubeadm token create --print-join-command
-
 
