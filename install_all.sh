ed4a6c53acddaa923fe7120cbd4f633479d318d6
vsaini44 <vishal@ap2v.com>
2021-06-10 12:53:17 +0530

    Update install_all.sh

--- install_all.sh
+++ install_all.sh
@@ -5,7 +5,7 @@ sudo apt install apt-transport-https ca-certificates curl software-properties-co
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 sudo apt update
-sudo apt-cache policy docker-ce -y
+sudo apt-cache policy docker-ce 
 sudo apt install docker-ce -y
 wget -q -O - https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 echo deb http://apt.kubernetes.io/ kubernetes-xenial main | sudo tee /etc/apt/sources.list.d/kubernetes.list
