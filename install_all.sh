e869a20b06620dc8a1fe6c1f9c002c1f88d6c13c
vsaini44 <vishal@ap2v.com>
2021-06-10 12:50:31 +0530

    Update install_all.sh

--- install_all.sh
+++ install_all.sh
@@ -5,10 +5,10 @@ sudo apt install apt-transport-https ca-certificates curl software-properties-co
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 sudo apt update
-apt-cache policy docker-ce -y
+sudo apt-cache policy docker-ce -y
 sudo apt install docker-ce -y
 wget -q -O - https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 echo deb http://apt.kubernetes.io/ kubernetes-xenial main | sudo tee /etc/apt/sources.list.d/kubernetes.list
-apt update
-apt install kubelet kubeadm -y
-sysctl net.bridge.bridge-nf-call-iptables=1
+sudo apt update
+sudo apt install kubelet kubeadm -y
+sudo sysctl net.bridge.bridge-nf-call-iptables=1
